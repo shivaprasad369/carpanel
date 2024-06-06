19655e3e3e6cb6026ea64d695d644ce73e6ce1ad
shivaprasad369 <122955366+shivaprasad369@users.noreply.github.com>
2024-06-07 02:35:11 +0530

    Add

--- src/Panel.jsx
+++ src/Panel.jsx
@@ -44,7 +44,7 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
         //        )
                useEffect(()=>{
 
-                axios.get('https://carsbackend-3oe0.onrender.com/api/v1/post').then((data)=>).catch((error)=>console.log(error))
+                axios.get('https://carsbackend-3oe0.onrender.com/api/v1/post').then((data)=> setData(data.data.data)).catch((error)=>console.log(error))
               },[]);
            return (
             <>
